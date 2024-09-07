b93adb6798da43bc89e7fb928093423dd2044e01
ahmedhamdi122 <ahmedhamdisaeed12@gmail.com>
2024-09-08 01:33:38 +0300

    edite

--- Asset.API/Controllers/AuthenticationController.cs
+++ Asset.API/Controllers/AuthenticationController.cs
@@ -59,177 +59,166 @@ namespace Asset.API.Controllers
             var user = await _userManager.FindByNameAsync(userObj.Username);
 
             if (user == null)
-                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User does not exists!" });
-
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "UserNotFound", Message = "There is no account associated with this username", MessageAr = "لا يوجد حساب مرتبط  باسم المستخدم " });
             var userpass = await _userManager.CheckPasswordAsync(user, userObj.PasswordHash);
             if (userpass == false)
-                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Invalid Password!" });
-
-
-            if (user != null && await _userManager.CheckPasswordAsync(user, userObj.PasswordHash))
-            {
-
-
-                var userRoles = await _userManager.GetRolesAsync(user);
-
-                var authClaims = new List<Claim>
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "invalidPassword", Message = "The password you entered is incorrect. Please try again.", MessageAr = "كلمة السر التي أدخلتها غير صحيحة,الرجاء المحاولة مرة أخرى" });
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var authClaims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, user.UserName),
                     new Claim(ClaimTypes.Email, user.Email),
                    // new Claim("key","value"),
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 };
-
-                foreach (var userRole in userRoles)
-                {
-                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
-                }
-                foreach (var claim in authClaims)
-                {
-                    await _userManager.AddClaimAsync(user, claim);
-
-                }
-                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
-
-                var token = new JwtSecurityToken(
-                    issuer: _configuration["JWT:ValidIssuer"],
-                    audience: _configuration["JWT:ValidAudience"],
-                    //      expires: DateTime.Now.AddHours(3),
-                    claims: authClaims,
-                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
-                    );
-
-
-                var id = user.Id;
-                var name = user.UserName;
-                var governorateId = user.GovernorateId;
-                var cityId = user.CityId;
-                var orgId = user.OrganizationId;
-                var subOrgId = user.SubOrganizationId;
-                var hospitalId = user.HospitalId;
-                var supplierId = user.SupplierId;
-                var commetieeMemberId = user.CommetieeMemberId;
-
-                var govName = user.GovernorateId > 0 ? _context.Governorates.Where(a => a.Id == user.GovernorateId).First().Name : "";
-                var cityName = user.CityId > 0 ? _context.Cities.Where(a => a.Id == user.CityId).First().Name : "";
-                var orgName = user.OrganizationId > 0 ? _context.Organizations.Where(a => a.Id == user.OrganizationId).First().Name : "";
-                var subOrgName = user.SubOrganizationId > 0 ? _context.SubOrganizations.Where(a => a.Id == user.SubOrganizationId).First().Name : "";
-                var govNameAr = user.GovernorateId > 0 ? _context.Governorates.Where(a => a.Id == user.GovernorateId).First().NameAr : "";
-                var cityNameAr = user.CityId > 0 ? _context.Cities.Where(a => a.Id == user.CityId).First().NameAr : "";
-                var orgNameAr = user.OrganizationId > 0 ? _context.Organizations.Where(a => a.Id == user.OrganizationId).First().NameAr : "";
-                var subOrgNameAr = user.SubOrganizationId > 0 ? _context.SubOrganizations.Where(a => a.Id == user.SubOrganizationId).First().NameAr : "";
-
-
-                var hospitalName = user.HospitalId > 0 ? _context.Hospitals.Where(a => a.Id == user.HospitalId).First().Name : "";
-
-
-
-                var hospitalNameAr = user.HospitalId > 0 ? _context.Hospitals.Where(a => a.Id == user.HospitalId).First().NameAr : "";
-                var hospitalCode = user.HospitalId > 0 ? _context.Hospitals.Where(a => a.Id == user.HospitalId).First().Code : "";
-                var roleNames = (from userRole in _context.UserRoles
-                                 join role in _roleManager.Roles on userRole.RoleId equals role.Id
-                                 where user.Id == userRole.UserId
-                                 select role);
-
-                if (supplierId > 0)
-                {
-                    var lstSuppliers = _context.Suppliers.Where(a => a.EMail == user.Email).ToList();
-                    if (lstSuppliers.Count > 0)
-                    {
-                        var supplierObj = lstSuppliers[0];
-                        Useremail = user.Email;
-                        userName = supplierObj.Name;
-                        userNameAr = supplierObj.NameAr;
-                    }
-                }
-                if (commetieeMemberId > 0)
-                {
-                    var lstMembers = _context.CommetieeMembers.Where(a => a.EMail == user.Email).ToList();
-                    if (lstMembers.Count > 0)
-                    {
-                        var memberObj = lstMembers[0];
-                        Useremail = user.Email;
-                        userName = memberObj.Name;
-                        userNameAr = memberObj.NameAr;
-                    }
-                }
-                else
-                {
-                    Useremail = user.Email;
-                    userName = user.UserName;
-                }
-
-                var lstSettings = _settingService.GetAll().ToList();
-                if (lstSettings.Count > 0)
-                {
-                    foreach (var item in lstSettings)
-                    {
-                        if (item.KeyName == "Institute")
-                        {
-                            strInsitute = item.KeyValue;
-                            strInsituteAr = item.KeyValueAr;
-                        }
-
-                        if (item.KeyName == "Logo")
-                            strLogo = item.KeyValue;
-
-                        if (item.KeyName == "PMAgency")
-                            isAgency = Convert.ToBoolean(item.KeyValue);
-
-                      
-
-                        if (item.KeyName == "IsOpenRequest")
-                            isOpenRequest = Convert.ToBoolean(item.KeyValue);
-
-            
-                        if (item.KeyName == "HospitalType")
-                            hospitalTypeNum = Convert.ToInt32(item.KeyValue);
-
-                    }
-                }
-
-                return Ok(new
-                {
-                    token = new JwtSecurityTokenHandler().WriteToken(token),
-                    email = Useremail,
-                    userName = userName,
-                    userNameAr = userNameAr,
-                    roles = userRoles,
-                    expiration = token.ValidTo,
-                    id = id,
-                    roleNames = roleNames,
-                    governorateId = governorateId,
-                    cityId = cityId,
-                    organizationId = orgId,
-                    subOrganizationId = subOrgId,
-                    hospitalId = hospitalId,
-                    supplierId = supplierId,
-                    commetieeMemberId = commetieeMemberId,
-                    govName = govName,
-                    cityName = cityName,
-                    orgName = orgName,
-                    subOrgName = subOrgName,
-                    hospitalName = hospitalName,
-                    govNameAr = govNameAr,
-                    cityNameAr = cityNameAr,
-                    orgNameAr = orgNameAr,
-                    subOrgNameAr = subOrgNameAr,
-                    hospitalNameAr = hospitalNameAr,
-                    hospitalCode = hospitalCode,
-                    strInsitute = strInsitute,
-                    strInsituteAr = strInsituteAr,
-                    strLogo = strLogo,
-                    isAgency = isAgency,
-                    isScrap = isScrap,
-                    isVisit = isVisit,
-                    isOpenRequest = isOpenRequest,
-                    isExternalFix = isExternalFix,
-                    canAdd = canAdd,
-                    hospitalTypeNum = hospitalTypeNum
-                });
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
             }
-            return Unauthorized();
-        }
+            foreach (var claim in authClaims)
+            {
+                await _userManager.AddClaimAsync(user, claim);
+
+            }
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                //      expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            var id = user.Id;
+            var name = user.UserName;
+            var governorateId = user.GovernorateId;
+            var cityId = user.CityId;
+            var orgId = user.OrganizationId;
+            var subOrgId = user.SubOrganizationId;
+            var hospitalId = user.HospitalId;
+            var supplierId = user.SupplierId;
+            var commetieeMemberId = user.CommetieeMemberId;
+
+            var govName = user.GovernorateId > 0 ? _context.Governorates.Where(a => a.Id == user.GovernorateId).First().Name : "";
+            var cityName = user.CityId > 0 ? _context.Cities.Where(a => a.Id == user.CityId).First().Name : "";
+            var orgName = user.OrganizationId > 0 ? _context.Organizations.Where(a => a.Id == user.OrganizationId).First().Name : "";
+            var subOrgName = user.SubOrganizationId > 0 ? _context.SubOrganizations.Where(a => a.Id == user.SubOrganizationId).First().Name : "";
+            var govNameAr = user.GovernorateId > 0 ? _context.Governorates.Where(a => a.Id == user.GovernorateId).First().NameAr : "";
+            var cityNameAr = user.CityId > 0 ? _context.Cities.Where(a => a.Id == user.CityId).First().NameAr : "";
+            var orgNameAr = user.OrganizationId > 0 ? _context.Organizations.Where(a => a.Id == user.OrganizationId).First().NameAr : "";
+            var subOrgNameAr = user.SubOrganizationId > 0 ? _context.SubOrganizations.Where(a => a.Id == user.SubOrganizationId).First().NameAr : "";
+
+
+            var hospitalName = user.HospitalId > 0 ? _context.Hospitals.Where(a => a.Id == user.HospitalId).First().Name : "";
+
+
+
+            var hospitalNameAr = user.HospitalId > 0 ? _context.Hospitals.Where(a => a.Id == user.HospitalId).First().NameAr : "";
+            var hospitalCode = user.HospitalId > 0 ? _context.Hospitals.Where(a => a.Id == user.HospitalId).First().Code : "";
+            var roleNames = (from userRole in _context.UserRoles
+                             join role in _roleManager.Roles on userRole.RoleId equals role.Id
+                             where user.Id == userRole.UserId
+                             select role);
+
+            if (supplierId > 0)
+            {
+                var lstSuppliers = _context.Suppliers.Where(a => a.EMail == user.Email).ToList();
+                if (lstSuppliers.Count > 0)
+                {
+                    var supplierObj = lstSuppliers[0];
+                    Useremail = user.Email;
+                    userName = supplierObj.Name;
+                    userNameAr = supplierObj.NameAr;
+                }
+            }
+            if (commetieeMemberId > 0)
+            {
+                var lstMembers = _context.CommetieeMembers.Where(a => a.EMail == user.Email).ToList();
+                if (lstMembers.Count > 0)
+                {
+                    var memberObj = lstMembers[0];
+                    Useremail = user.Email;
+                    userName = memberObj.Name;
+                    userNameAr = memberObj.NameAr;
+                }
+            }
+            else
+            {
+                Useremail = user.Email;
+                userName = user.UserName;
+            }
+
+            var lstSettings = _settingService.GetAll().ToList();
+            if (lstSettings.Count > 0)
+            {
+                foreach (var item in lstSettings)
+                {
+                    if (item.KeyName == "Institute")
+                    {
+                        strInsitute = item.KeyValue;
+                        strInsituteAr = item.KeyValueAr;
+                    }
+
+                    if (item.KeyName == "Logo")
+                        strLogo = item.KeyValue;
+
+                    if (item.KeyName == "PMAgency")
+                        isAgency = Convert.ToBoolean(item.KeyValue);
+
+
+
+                    if (item.KeyName == "IsOpenRequest")
+                        isOpenRequest = Convert.ToBoolean(item.KeyValue);
+
+
+                    if (item.KeyName == "HospitalType")
+                        hospitalTypeNum = Convert.ToInt32(item.KeyValue);
+
+                }
+            }
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                email = Useremail,
+                userName = userName,
+                userNameAr = userNameAr,
+                roles = userRoles,
+                expiration = token.ValidTo,
+                id = id,
+                roleNames = roleNames,
+                governorateId = governorateId,
+                cityId = cityId,
+                organizationId = orgId,
+                subOrganizationId = subOrgId,
+                hospitalId = hospitalId,
+                supplierId = supplierId,
+                commetieeMemberId = commetieeMemberId,
+                govName = govName,
+                cityName = cityName,
+                orgName = orgName,
+                subOrgName = subOrgName,
+                hospitalName = hospitalName,
+                govNameAr = govNameAr,
+                cityNameAr = cityNameAr,
+                orgNameAr = orgNameAr,
+                subOrgNameAr = subOrgNameAr,
+                hospitalNameAr = hospitalNameAr,
+                hospitalCode = hospitalCode,
+                strInsitute = strInsitute,
+                strInsituteAr = strInsituteAr,
+                strLogo = strLogo,
+                isAgency = isAgency,
+                isScrap = isScrap,
+                isVisit = isVisit,
+                isOpenRequest = isOpenRequest,
+                isExternalFix = isExternalFix,
+                canAdd = canAdd,
+                hospitalTypeNum = hospitalTypeNum
+            });
+        
+          
+    } 
 
 
 
@@ -252,7 +241,6 @@ namespace Asset.API.Controllers
                  {"token", token }
 
              };
-
             var callback = QueryHelpers.AddQueryString(forgotPasswordModel.ClientURI, param);
             var hash = callback.Split("#");
             var query = hash[0];
