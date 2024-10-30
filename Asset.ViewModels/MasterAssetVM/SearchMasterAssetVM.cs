1057597afb960b64034efc3a6e81d9b11286e38e
ahmedhamdi122 <ahmedhamdisaeed12@gmail.com>
2024-10-30 17:02:37 +0300

    editDto

--- Asset.ViewModels/MasterAssetVM/SearchMasterAssetVM.cs
+++ Asset.ViewModels/MasterAssetVM/SearchMasterAssetVM.cs
@@ -9,16 +9,13 @@ namespace Asset.ViewModels.MasterAssetVM
    public class SearchSortMasterAssetVM
     {
         public int SortOrder { get; set; }    
-        public string? SortFiled { get; set; }
+        public string SortFiled { get; set; }
         public int ECRIId { get; set; }
         public int OriginId { get; set; }
         public int BrandId { get; set; }
 
         public int CategoryId { get; set; }
         public int SubCategoryId { get; set; }
-
-
-
         public string Code { get; set; }
         public string ModelNumber { get; set; }
         public string AssetName { get; set; }
